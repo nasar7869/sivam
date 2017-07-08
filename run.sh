06d35e7ed04e679ac0eda0f4c687c205e444f182
nasar7869 <nasarbasha786p@gmail.com>
2017-07-08 22:24:00 +0530

    run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod 400 /home/ubuntu/jenkins/tomcattest.pem
+
+ssh -i /home/ubuntu/jenkins/tomcattest.pem ubuntu@34.227.76.107
+
+
+scp -i /home/ubuntu/jenkins/tomcattest.pem ${WORKSPACE}/*.war ubuntu@34.227.76.107:/var/lib/tomcat7/webapps/
+
+ssh -i /home/ubuntu/jenkins/tomcattest.pem ubuntu@34.227.76.107 sudo service tomcat7 restart
+
